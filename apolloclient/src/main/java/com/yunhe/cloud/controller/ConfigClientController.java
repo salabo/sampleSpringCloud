ccdbea5ae6908a119d51ee7395820e47ac8ccd1b
zhangbo <449625455@qq.com>
2019-06-30 11:24:22 +0800

    删除target文件夹

--- apolloclient/src/main/java/com/yunhe/cloud/controller/ConfigClientController.java
+++ apolloclient/src/main/java/com/yunhe/cloud/controller/ConfigClientController.java
@@ -1,7 +1,13 @@
 package com.yunhe.cloud.controller;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
+import com.ctrip.framework.apollo.ConfigService;
+import com.ctrip.framework.apollo.model.ConfigChange;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +18,25 @@ public class ConfigClientController {
 
     @Value("${config_info}")
     private String config;
+
+
+    @GetMapping("/cg")
+    public String apploConfig(){
+        Config config = ConfigService.getAppConfig();
+        config.addChangeListener(new ConfigChangeListener() {
+            @Override
+            public void onChange(ConfigChangeEvent changeEvent) {
+                for (String key : changeEvent.changedKeys()) {
+                    ConfigChange change = changeEvent.getChange(key);
+                    System.out.println(String.format(
+                            "Found change - key: %s, oldValue: %s, newValue: %s, changeType: %s",
+                            change.getPropertyName(), change.getOldValue(),
+                            change.getNewValue(), change.getChangeType()));
+                }
+            }
+        });
+        return config.toString();
+    }
 
     @RequestMapping
     public String getConfigInfo(){
